6da00c07a43b0b73d86bc0b401caac6cbfe419a4
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2020-11-15 22:35:40 +0900

    add FindSharedLines.read_file_into_set()

--- lib/find_shared_lines.rb
+++ lib/find_shared_lines.rb
@@ -1,6 +1,16 @@
-require "find_shared_lines/version"
+# frozen_string_literal: true
+
+require 'set'
+require 'find_shared_lines/version'
 
 module FindSharedLines
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.read_lines_from(file)
+    File.open(file).readlines.map(&:chomp)
+  end
+
+  def self.read_file_into_set(file)
+    Set.new(read_lines_from(file))
+  end
 end
--- spec/find_shared_lines_spec.rb
+++ spec/find_shared_lines_spec.rb
@@ -3,7 +3,29 @@ RSpec.describe FindSharedLines do
     expect(FindSharedLines::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.read_file_into_set' do
+    filepath = 'fixtures/a.txt'
+
+    it 'returns a set' do
+      set = FindSharedLines.read_file_into_set(filepath)
+      expect(set).to be_a(Set)
+    end
+
+    it 'return a set of unique lines' do
+      expected = [
+        'only in a 1',
+        'only in a 2',
+        'in a and b 1',
+        'in a and b 2',
+        'in a and c 1',
+        'in a and c 2',
+        'in a, b and c 1',
+        'in a, b and c 2'
+      ].sort
+
+      set = FindSharedLines.read_file_into_set(filepath)
+
+      expect(set.to_a.sort).to eq(expected)
+    end
   end
 end
